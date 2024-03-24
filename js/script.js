8cb86deee3a5955924fff9694306af3a3b19a7c8
Unppear <107591895+Unppear@users.noreply.github.com>
2024-03-24 14:21:36 -0300

    v.1.3

--- js/script.js
+++ js/script.js
@@ -2,5 +2,12 @@
 
 /*---Função prime---*/
 $(window).on('load', function() {
-    
+    /*---Popup---*/ 
+    $('#open_contact').on('click', function() {
+        $('.background_popup').fadeIn('500', () => {});
+        $('.background_popup').css('display', 'flex');
+    });
+    $('.close').on('click', () => {
+        $('.background_popup').fadeOut('300', () => {});
+    })
 });
