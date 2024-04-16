8986fa0b1913804bd0df5d8811a00b35f08b8b9a
Unppear <107591895+Unppear@users.noreply.github.com>
2024-04-16 13:22:27 -0300

    v2.3

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ $(window).on('load', function() {
         popupBackground.fadeIn('500', () => {});
         popupBackground.css('display', 'flex');
     });
-    $('.close').on('click', () => {
+    $('#close_popup').on('click', () => {
         popupBackground.fadeOut('500', () => {});
     })
     /*---Container escondido---*/
@@ -46,13 +46,49 @@ $(window).on('load', function() {
         boxlinkThree.fadeOut();
     });
     /*---Card---*/
-    var card_background = $('.background_card'), open_centroPop = $('#open_centroPop'), card_centroPop = $('.centroPop_card');
+    var card_background = $('.background_card'), close_centroPop = $('#close_centroPop'), open_centroPop = $('#open_centroPop'), card_centroPop = $('.centroPop_card'), 
+    open_cras = $('#open_cras'), cras_card = $('.cras_card'), close_cras = $('#close_cras'), open_creas = $('#open_creas'), card_creas = $('.creas_card'), 
+    close_creas = $('#close_creas');
 
+    /*--Centro Pop---*/
     open_centroPop.on('click', () => {
         card_background.fadeIn('500', () => {});
         card_background.css('display', 'flex');
 
         card_centroPop.fadeIn('500', () => {});
         card_centroPop.css('display', 'grid');
+    });
+
+    close_centroPop.on('click', () => {
+        card_background.fadeOut('500', () => {});
+        card_centroPop.fadeOut('500', () => {});
+    });
+
+    /*--Cras---*/
+    open_cras.on('click', () => {
+        card_background.fadeIn('500', () => {});
+        card_background.css('display', 'flex');
+
+        cras_card.fadeIn('500', () => {});
+        cras_card.css('display', 'grid');
+    });
+
+    close_cras.on('click', () => {
+        card_background.fadeOut('500', () => {});
+        cras_card.fadeOut('500', () => {});
+    });
+
+    /*---Creas---*/
+    open_creas.on('click', () => {
+        card_background.fadeIn('500', () => {});
+        card_background.css('display', 'flex');
+
+        card_creas.fadeIn('500', () => {});
+        card_creas.css('display', 'grid');
+    });
+
+    close_creas.on('click', () => {
+        card_background.fadeOut('500', () => {});
+        card_creas.fadeOut('500', () => {});
     });
 });
